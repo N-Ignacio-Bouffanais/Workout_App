54a2e14e00ff70290aa4bd715f6ac9497630051c
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-03-24 20:32:14 -0300

    [setup] reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
-import 'pages/login_page.dart';
-import 'pages/profile_page.dart';
 
 void main() {
-  runApp(const RootPage());
+  runApp(const MyApp());
 }
 
-class RootPage extends StatefulWidget {
-  const RootPage({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<RootPage> createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  List<Widget> pages = const [
-    HomePage(),
-    LoginPage(),
-    ProfilePage(),
-  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Workout App"),
-      ),
-      body: pages[0],
-    );
+    return Container();
   }
 }
